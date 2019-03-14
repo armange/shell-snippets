f4ae9b006fd8390d1ca16e82466e1194cc899455
Diego Armange Costa <diego.armange.costa@gmail.com>
2019-03-14 12:08:00 -0300

    Fixing local branch name reading.

--- related_to_git/git-branch-info.sh
+++ related_to_git/git-branch-info.sh
@@ -6,7 +6,7 @@ find_any_branch() {
 
 get_commits_behind() {
     if [ -n "$branch" ]; then
-       echo -e '\u2193'`git rev-list --count origin/$branch --not $branch`
+       echo -e '\u2193'`git rev-list --count origin/$branch --not HEAD`
     else
        echo ""
     fi
@@ -14,7 +14,7 @@ get_commits_behind() {
 
 get_commits_ahead() {
     if [ -n "$branch" ]; then
-        echo -e '\u2191'`git rev-list --count $branch --not origin/$branch`
+        echo -e '\u2191'`git rev-list --count HEAD --not origin/$branch`
     else
         echo ""
     fi
@@ -35,5 +35,13 @@ get_branch_info() {
         echo ""
     fi
 }
+
+
+#if [ "$color_prompt" = yes ]; then
+#    PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[33m\]$(get_branch_info)\[\033[00m\]\$ '
+#else
+#    PS1='${debian_chroot:+($debian_chroot)}\u@\h:\w\$ '
+#fi
+
 
 (get_branch_info)
